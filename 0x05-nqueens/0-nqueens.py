5deecf1d91c337b4dcc39c03b863c51b9ebae71b
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-29 21:09:43 -0400

    Still by the Rabba.

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """The Queen Module."""
-import sys
+from sys import argv
 
 
 def is_safe(board, row, col, n):
@@ -49,23 +49,17 @@ def solve_nqueens(n):
         print(solution)
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+if len(argv) > 2 or len(argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+if not argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+if int(argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    solve_nqueens(n)
-
-
-if __name__ == "__main__":
-    main()
+n_queen = int(argv[1])
+solve_nqueens(n_queen)
