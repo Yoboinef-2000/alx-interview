8826682cf205d78c0ae1a19c966f37f0fd359bc1
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-02 15:46:03 -0400

    May the Force with you.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,21 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-const request = require ('request')
+const request = require('request');
 
-const theAPI = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`
-
+const theAPI = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
 request(theAPI, { json: true }, (err, res, body) => {
+  if (err) {
+    return console.error('Error Here! Error Here!:', err);
+  }
+  const allTheCharacters = body.characters;
 
-    const allTheCharacters = body.characters;
-
-    for (const aCharacter of allTheCharacters) {
-        request(aCharacter, { json: true }, (err, res, characterBody) => {
-          console.log(characterBody.name);
-        });
+  for (const aCharacter of allTheCharacters) {
+    request(aCharacter, { json: true }, (err, res, characterBody) => {
+      if (err) {
+        return console.error('Error Here! Error Here!', err);
       }
+      console.log(characterBody.name);
+    });
+  }
 });
