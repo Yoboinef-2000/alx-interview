01e44ec89d5426cf605b471cd33ed6874373c322
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-15 04:04:24 -0400

    Rotating a 2D matrix.

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix 90 degrees clockwise."""
+    theN = len(matrix)
+
+    i = 0
+
+    while i < theN:
+        j = i
+        while j < theN:
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            j += 1
+        i += 1
+
+    for z in range(theN):
+        matrix[z].reverse()
