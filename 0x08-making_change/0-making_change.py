b01fee2dabd36bc38ad9e76ed9a5b800c1fc8163
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-19 15:48:26 -0400

    Making Change.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
-from collections import deque
 """Change comes from within."""
 
 
-def makeChange(coins, total):
-    """THe change maker."""
-    if total == 0:
+def makeChange(coinValues, targetTotal):
+    """Making Change."""
+    if targetTotal <= 0:
         return 0
-    queue = deque([(0, 0)])
-    visited = set([0])
 
-    while queue:
-        current_sum, num_coins = queue.popleft()
+    fewestCoins = [float('inf')] * (targetTotal + 1)
+    fewestCoins[0] = 0
 
-        for coin in coins:
-            next_sum = current_sum + coin
+    for coin in coinValues:
+        for currentAmount in range(coin, targetTotal + 1):
+            fewestCoins[currentAmount] = min(fewestCoins[currentAmount],
+                                             fewestCoins[
+                                                 currentAmount - coin] + 1)
 
-            if next_sum == total:
-                return num_coins + 1
-
-            if next_sum < total and next_sum not in visited:
-                visited.add(next_sum)
-                queue.append((next_sum, num_coins + 1))
-
-    return -1
+    return fewestCoins[targetTotal] if fewestCoins[targetTotal] != \
+        float('inf') else -1
